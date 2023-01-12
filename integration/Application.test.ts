6398cefd77a7608d0998d1f33b45794d28135f67
denis-rossati <denis.rossatiramos@gmail.com>
2023-01-11 21:17:07 -0300

    Add to-do integration tests

---
+++ integration/Application.test.ts
@@ -0,0 +1,9 @@
+describe('The application', () => {
+	it.todo('Should send location all fields filled.');
+	it.todo('Should send location with null optional fields.');
+	it.todo('Should send location retrieved from cache.');
+	it.todo('Should ignore message if it is not a valid JSON.');
+	it.todo('Should ignore message if IP is invalid.');
+	it.todo('Should ignore message if external resourcers send unrecognizable data.');
+	it.todo('Should send location to output topic in form of JSON.');
+});
